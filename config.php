ea56f116443111d4244720fa23c6bcf5fd042cf6
Cyril MAGUIRE <github@ecyseo.net>
2014-01-19 18:16:12 +0100

    Add timeout param

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * Plugin logInMyPluxml
  *
  * @package	PLX
- * @version	1.2
+ * @version	1.3
  * @date	19/01/2014
  * @author	Cyril MAGUIRE
  **/
@@ -14,6 +14,7 @@ if(!empty($_POST)) {
 	$plxPlugin->setParam('mnuDisplay', $_POST['mnuDisplay'], 'numeric');
 	$plxPlugin->setParam('mnuName', $_POST['mnuName'], 'string');
 	$plxPlugin->setParam('mnuPos', $_POST['mnuPos'], 'numeric');
+	$plxPlugin->setParam('timeout', $_POST['timeout'], 'numeric');
 	$plxPlugin->saveParams();
 	header('Location: parametres_plugin.php?p=logInMyPluxml');
 	exit;
@@ -21,6 +22,7 @@ if(!empty($_POST)) {
 $mnuDisplay =  $plxPlugin->getParam('mnuDisplay')=='' ? 1 : $plxPlugin->getParam('mnuDisplay');
 $mnuName =  $plxPlugin->getParam('mnuName')=='' ? $plxPlugin->getLang('L_DEFAULT_MENU_NAME') : $plxPlugin->getParam('mnuName');
 $mnuPos =  $plxPlugin->getParam('mnuPos')=='' ? 2 : $plxPlugin->getParam('mnuPos');
+$timeout =  $plxPlugin->getParam('timeout')=='' ? 60 : $plxPlugin->getParam('timeout');
 
 ?>
 
@@ -34,6 +36,8 @@ $mnuPos =  $plxPlugin->getParam('mnuPos')=='' ? 2 : $plxPlugin->getParam('mnuPos
 		<?php plxUtils::printInput('mnuName',$mnuName,'text','20-20') ?>
 		<p class="field"><label for="id_mnuPos"><?php $plxPlugin->lang('L_MENU_POS') ?>&nbsp;:</label></p>
 		<?php plxUtils::printInput('mnuPos',$mnuPos,'text','2-5') ?>
+		<p class="field"><label for="id_timeout"><?php $plxPlugin->lang('L_TIMEOUT') ?>&nbsp;:</label></p>
+		<?php plxUtils::printInput('timeout',$timeout,'text','2-5') ?>
 		<p>
 			<?php echo plxToken::getTokenPostMethod() ?>
 			<input type="submit" name="submit" value="<?php $plxPlugin->lang('L_SAVE') ?>" />

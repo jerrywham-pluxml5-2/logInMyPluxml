cebc4413afb4e7e77f6e124d76f14d87b6f3bc66
Cyril MAGUIRE <github@ecyseo.net>
2014-01-21 10:53:49 +0100

    Add L_TIMEOUT

--- lang/fr.php
+++ lang/fr.php
@@ -8,6 +8,7 @@ $LANG = array(
 'L_MENU_DISPLAY'			=> 'Afficher le menu de la page de contact',
 'L_MENU_TITLE'		    	=> 'Titre du menu',
 'L_MENU_POS'		    	=> 'Position du menu',
+'L_TIMEOUT'		    		=> 'Temps de déconnexion en minutes (laissez à 0 pour un temps de connexion infini)',
 'L_SAVE'					=> 'Enregistrer',
 
 'L_DEFAULT_MENU_NAME'		=> 'Déconnexion',
